fdeefd00714aee1c70225e5c89dabbe01560a598
Victor Van Weyenberg <victorvanweyenb@gmail.com>
2019-05-06 21:34:07 +0200

    Implement reset method.

--- src/io/MeasurementWriter.java
+++ src/io/MeasurementWriter.java
@@ -2,6 +2,7 @@ package io;
 
 import measure.Measurement;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -18,4 +19,7 @@ public class MeasurementWriter {
         }
     }
 
+    public static void reset() {
+        new File(measurementFileName).delete();
+    }
 }
