42b8829913085f478795558f9873ed2d0ba0752e
ReznovvV <victorvanweyenb@gmail.com>
2019-05-06 15:44:05 +0200

    Edit StartUp.

--- src/main/StartUp.java
+++ src/main/StartUp.java
@@ -5,48 +5,63 @@
  */
 package main;
 
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import beads.BeadsTemplate;
 import jsyn.JSynTemplate;
-import net.beadsproject.beads.core.io.JavaSoundAudioIO;
+import measure.Measurer;
+import measure.Parameter;
 
 /**
- *
  * @author Reznov
  */
 public class StartUp {
-    
-    private static Object[][] data = new Object[][] {
-        {"Hobbyist", 4, 0, 0, 0},
-        {"Live band", 10, 0, 2, 5},
-        {"Metal band", 40, 40, 3, 15},
-        {"Jazz concert", 50, 50, 5, 20},
-        {"Video productie", 150, 150, 0, 0}
-    };
-    
-    private static Template[] benchmarks = new Template[] {
-        new JSynTemplate(),
-        new BeadsTemplate(),
+
+    private final static List<Parameter> parameters = new ArrayList<>();
+
+    static {
+        int voices, voicesToEQandComp, effects, voicesToEffects;
+        for (voices = 4; voices <= 150; voices += 2) {
+            for (voicesToEQandComp = voices / 2; voicesToEQandComp <= voices; voicesToEQandComp++) {
+                for (effects = 0; effects <= 5; effects++) {
+                    if (effects > 0) {
+                        for (voicesToEffects = 1; voicesToEffects <= voices / 2; voicesToEffects++) {
+                            parameters.add(new Parameter(voices, voicesToEQandComp, effects, voicesToEffects));
+                        }
+                    } else {
+                        parameters.add(new Parameter(voices, voicesToEQandComp, effects, 0));
+                    }
+                }
+            }
+        }
+    }
+
+    private static Template[] templates = new Template[]{
+            new BeadsTemplate(),
+            new JSynTemplate(),
     };
 
     public static void main(String[] args) throws InterruptedException {
         new StartUp();
+        // System.out.println(System.getProperty("java.library.path"));
+        //parameters.stream().filter(parameter -> parameter.getVoices() == 4).forEach(System.out::println);
         // JavaSoundAudioIO.printMixerInfo();
     }
-    
+
     public StartUp() throws InterruptedException {
         Set<Thread> firstSet = Thread.getAllStackTraces().keySet();
         firstSet.stream().map(Thread::getName).filter(thread -> thread.equals("Thread-0")).forEach(System.out::println);
-        for (Object[] args : data) {
-            for (Template benchmark : benchmarks) {
+        for (Template benchmark : templates) {
+            for (Parameter parameter : parameters) {
                 benchmark.reset();
-                benchmark.setup((int) args[1], (int) args[2], (int) args[3], (int) args[4]);
-                System.out.printf("%s: Measuring case \"%s\".%n", benchmark.getName(), args[0]);
+                benchmark.setup(parameter.getVoices(), parameter.getVoicesToEQandComp(), parameter.getEffects(), parameter.getVoicesToEffects());
+                System.out.printf("%s: Measuring case %s.%n", benchmark.getName(), parameter);
                 TimeUnit.SECONDS.sleep(1);
                 benchmark.run();
+                Measurer.measure(parameter);
                 TimeUnit.SECONDS.sleep(2);
                 benchmark.stop();
                 benchmark.tearDown();
