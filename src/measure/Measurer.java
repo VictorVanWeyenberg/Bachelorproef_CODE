4c26f6bf66fd32b9528ecdadb7f0901f94837682
Victor Van Weyenberg <victorvanweyenb@gmail.com>
2019-05-07 20:05:52 +0200

    Implement measuring method using BASH.

--- src/measure/Measurer.java
+++ src/measure/Measurer.java
@@ -1,16 +1,24 @@
 package measure;
 
 import io.MeasurementWriter;
+import main.StartUp;
 import org.hyperic.sigar.CpuPerc;
 import org.hyperic.sigar.Mem;
 import org.hyperic.sigar.Sigar;
 import org.hyperic.sigar.SigarException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.concurrent.TimeUnit;
 
 public class Measurer {
 
     private static final Sigar sigar = new Sigar();
     private static CpuPerc cpuMeasurer;
     private static Mem memMeasurer;
+
+    private static final String BASH_COMMAND = "top -b -n50 | grep %d | awk '{ print $9 \" \" $10 }'";
 
     private static double getCpuLoadPercentage() {
         if (cpuMeasurer == null) {
@@ -34,9 +42,32 @@ public class Measurer {
         return memMeasurer.getUsedPercent();
     }
 
-    public static void measure(Parameter parameter) {
-        Measurement measurement = new Measurement(parameter, getCpuLoadPercentage(), getMemoryLoadPercentae());
-        MeasurementWriter.write(measurement);
+    public static void measureSigar(int ID, Parameter parameter) throws InterruptedException {
+        for (int index = 0; index < 50; index++) {
+            TimeUnit.MILLISECONDS.sleep(10);
+            Measurement measurement = new Measurement(ID, index, parameter, getCpuLoadPercentage(), getMemoryLoadPercentae());
+            MeasurementWriter.write(measurement);
+            System.out.println(measurement);
+        }
+    }
+
+    public static void measureTop(int ID, Parameter parameter) throws InterruptedException, IOException {
+        int index = 0;
+        String[] command = new String[] {"/bin/sh", "-c", String.format(BASH_COMMAND, StartUp.getPID())};
+        Process process; BufferedReader reader; String output;
+        Runtime runtime = Runtime.getRuntime();
+        while (index < 50) {
+            process = runtime.exec(command);
+            reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            output = reader.readLine();
+            reader.close();
+            process.waitFor();
+            double cpuLoad = Float.valueOf(output.replaceAll(",", ".").split(" ")[0]);
+            double memLoad = Float.valueOf(output.replaceAll(",", ".").split(" ")[1]);
+            Measurement measurement = new Measurement(ID, index, parameter, cpuLoad, memLoad);
+            MeasurementWriter.write(measurement);
+            index++;
+        }
     }
 
 }
