4c194d5d079149476885770b2df2fdae3d78c8a6
Victor Van Weyenberg <victorvanweyenb@gmail.com>
2019-05-07 20:00:31 +0200

    Add file and stream management.

--- src/io/MeasurementWriter.java
+++ src/io/MeasurementWriter.java
@@ -9,17 +9,33 @@ import java.io.IOException;
 
 public class MeasurementWriter {
 
-    private static final String measurementFileName = "measurements.csv";
+    private static final String CSV = ".csv";
+    private static FileWriter writer;
 
-    public static void write(Measurement measurement) {
-        try (FileWriter writer = new FileWriter(measurementFileName, true)) {
-            writer.write(measurement.toString());
+    public static void open(String fileName) {
+        try {
+            writer = new FileWriter(String.format("%s%s", fileName, CSV));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static void reset() {
-        new File(measurementFileName).delete();
+    public static void write(Measurement measurement) {
+        try {
+            writer.write(measurement.toString().endsWith("\n") ?
+                    measurement.toString() :
+                    new StringBuilder(measurement.toString()).append("\n").toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void flush() {
+        try {
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
